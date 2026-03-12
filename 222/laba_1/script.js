723e7f2b1e6d60c6be7b2b6c78b67c0bb69debd1
kolia kadyuk <kokakola2210@gmail.com>
2026-03-12 08:53:11 +0200

    reworked laba1

--- 222/laba_1/script.js
+++ 222/laba_1/script.js
@@ -1,14 +1,12 @@
-// 1. Використовуємо querySelector для доступу до h1
+
 const title = document.querySelector('#main-title');
 
-// 2. Виводимо "Hello world!" в елемент h1
 title.textContent = "Hello world!";
 
-// 3. Отримуємо доступ до кнопки
+//доступ до кнопки
 const btn = document.querySelector('#action-button');
 
-// 4. Додаємо функцію на івент onmousemove
+//onmousemove
 btn.onmousemove = function() {
-    // Виводимо ім'я студента в консоль як warn
     console.warn("Студент: Коля");
 };
